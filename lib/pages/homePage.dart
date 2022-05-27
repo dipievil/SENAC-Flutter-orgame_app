0420a7cac668b96f9dbabb26fc723bef7d5116ac
Diego Ritzel <dipi.evil@gmail.com>
2022-05-27 20:18:58 -0300

    Use method oh HomePage

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:orgame_app/pages/consolesPage.dart';
+import 'package:orgame_app/pages/gamesPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,46 +17,42 @@ class HomePage extends StatelessWidget {
           child: Column(
             children: [
               Flexible(
-                child: SizedBox(
-                  child: FractionallySizedBox(
-                    heightFactor: 0.9,
-                    widthFactor: 0.9,
-                    alignment: FractionalOffset.center,
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            imagePath + "consoles/nes.jpg",
-                            fit: BoxFit.fitHeight,
-                          ),
-                          const Text("Consoles")
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                child: showHomeBox(context, "consoles/nes.jpg", "Consoles",
+                    const ConsolesPage()),
               ),
               Flexible(
-                child: SizedBox(
-                  child: FractionallySizedBox(
-                    heightFactor: 0.9,
-                    widthFactor: 0.9,
-                    alignment: FractionalOffset.center,
-                    child: Container(
-                        alignment: Alignment.center,
-                        child: Column(
-                          children: [
-                            Image.asset(imagePath + "games/alexkidd.jpg",
-                                fit: BoxFit.contain),
-                            const Text("Games")
-                          ],
-                        )),
-                  ),
-                ),
+                child: showHomeBox(
+                    context, "games/alexkidd.jpg", "Games", const GamesPage()),
               ),
             ],
           ),
         ));
+  }
+
+  SizedBox showHomeBox(
+      BuildContext context, String image, String label, StatelessWidget page) {
+    return SizedBox(
+      child: FractionallySizedBox(
+        heightFactor: 0.9,
+        widthFactor: 0.9,
+        alignment: FractionalOffset.center,
+        child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => page));
+                  },
+                  child: Image.asset(imagePath + image, fit: BoxFit.fill),
+                ),
+                Text(label)
+              ],
+            )),
+      ),
+    );
   }
 }
