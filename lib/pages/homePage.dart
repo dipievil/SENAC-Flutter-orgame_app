c44655d2d7a882d7b8d184c3508f035e70479b22
Diego Ritzel <dipi.evil@gmail.com>
2022-05-27 20:00:27 -0300

    Adjust images and title on home page

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  static const imagePath = "assets/images/";
 
   @override
   Widget build(BuildContext context) {
@@ -10,30 +13,48 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
-            children: const [
+            children: [
               Flexible(
                 child: SizedBox(
                   child: FractionallySizedBox(
-                    heightFactor: 1,
+                    heightFactor: 0.9,
                     widthFactor: 0.9,
                     alignment: FractionalOffset.center,
-                    child: Center(child: Text("Azul")),
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            imagePath + "consoles/nes.jpg",
+                            fit: BoxFit.fitHeight,
+                          ),
+                          const Text("Consoles")
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
               Flexible(
                 child: SizedBox(
                   child: FractionallySizedBox(
-                    heightFactor: 1,
+                    heightFactor: 0.9,
                     widthFactor: 0.9,
                     alignment: FractionalOffset.center,
-                    child: Center(child: Text("Vermelho")),
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Image.asset(imagePath + "games/alexkidd.jpg",
+                                fit: BoxFit.contain),
+                            const Text("Games")
+                          ],
+                        )),
                   ),
                 ),
               ),
             ],
           ),
         ));
-    //return Container(color: Colors.blue);
   }
 }
