c0a96f98ef513877890d9d80d72ce3e672c84fd6
Diego Ritzel <dipi.evil@gmail.com>
2022-05-27 19:03:45 -0300

    Add start page

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -5,6 +5,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+        appBar: AppBar(title: const Text("OrGame")),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            children: const [
+              Flexible(
+                child: SizedBox(
+                  child: FractionallySizedBox(
+                    heightFactor: 1,
+                    widthFactor: 0.9,
+                    alignment: FractionalOffset.center,
+                    child: Center(child: Text("Azul")),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: SizedBox(
+                  child: FractionallySizedBox(
+                    heightFactor: 1,
+                    widthFactor: 0.9,
+                    alignment: FractionalOffset.center,
+                    child: Center(child: Text("Vermelho")),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+    //return Container(color: Colors.blue);
   }
 }
