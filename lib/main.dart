edd2aa77acecd6ff18523c80585b5ac07dde2a07
Diego Ritzel <dipi.evil@gmail.com>
2022-05-27 21:16:13 -0300

    Add class asset detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:orgame_app/pages/homePage.dart';
+import 'package:orgame_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
