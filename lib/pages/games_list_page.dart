d653cecd5350d8e0fe684a0b2de020f680afd227
Diego Ritzel <dipi.evil@gmail.com>
2022-05-28 09:58:30 -0300

    Add link to the details game page

--- lib/pages/games_list_page.dart
+++ lib/pages/games_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:orgame_app/pages/game_detail_page.dart';
 
 class GamesPage extends StatelessWidget {
   const GamesPage({Key? key}) : super(key: key);
@@ -11,40 +12,52 @@ class GamesPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("All Games"),
       ),
-      body: _listGames(),
+      body: _listGames(context),
     );
   }
 
-  Widget _listGames() {
+  Widget _listGames(context) {
     return ListView(
       children: [
-        _gameDetail("Allex Kidd In Miracle World", imagePath + "alexkidd.jpg"),
-        _gameDetail("Castlevania X", imagePath + "castlevania-draculax.jpg"),
-        _gameDetail("Castlevania", imagePath + "Castlevania.png"),
+        _gameDetail("Allex Kidd In Miracle World", imagePath + "alexkidd.jpg",
+            context, const GameDetailPage()),
+        _gameDetail("Castlevania X", imagePath + "castlevania-draculax.jpg",
+            context, const GameDetailPage()),
+        _gameDetail("Castlevania", imagePath + "Castlevania.png", context,
+            const GameDetailPage()),
         _gameDetail(
-            "CastlevaniaBlodlines", imagePath + "CastlevaniaBloodlines.png")
+            "CastlevaniaBlodlines",
+            imagePath + "CastlevaniaBloodlines.png",
+            context,
+            const GameDetailPage())
       ],
     );
   }
 
-  Widget _gameDetail(gameName, imagePath) {
-    return Row(
-      children: [
-        Image.asset(imagePath, width: 100, height: 100, fit: BoxFit.contain),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "$gameName",
-              style: const TextStyle(
-                  backgroundColor: Colors.white,
-                  color: Colors.black,
-                  fontSize: 16),
+  Widget _gameDetail(gameName, imagePath, context, StatelessWidget page) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) => page));
+      },
+      child: Row(
+        children: [
+          Image.asset(imagePath, width: 100, height: 100, fit: BoxFit.contain),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "$gameName",
+                style: const TextStyle(
+                    backgroundColor: Colors.white,
+                    color: Colors.black,
+                    fontSize: 16),
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
