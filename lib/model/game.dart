6495990df658bb566cc06022266b29ca5ab0f063
Diego Ritzel <dipi.evil@gmail.com>
2022-05-28 14:32:51 -0300

    Fix game model

--- lib/model/game.dart
+++ lib/model/game.dart
@@ -1,6 +1,13 @@
 class Game {
-  final String name;
   final int id;
+  final String name;
+  final String console;
+  final String year;
+  final String genre;
+  final String designer;
+  final String developer;
+  final String imageFile;
 
-  Game(this.name, this.id);
+  Game(this.id, this.name, this.console, this.year, this.genre, this.designer,
+      this.developer, this.imageFile);
 }
